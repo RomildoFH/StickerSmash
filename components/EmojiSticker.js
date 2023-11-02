9aba98628d4bb1c1809603152b37136754df3cb2
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-11-02 09:45:56 -0300

    feat: criada funcionalidade de pan para mover o emoji

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { View, Image } from 'react-native';
-import { TapGestureHandler } from 'react-native-gesture-handler';
+import { PanGestureHandler, State, TapGestureHandler } from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -8,10 +9,29 @@ import Animated, {
 } from 'react-native-reanimated';
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
-
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0); // Adicione translateX compartilhado
+  const translateY = useSharedValue(0); // Adicione translateY compartilhado
+
+  // Manipulador de gesto para arrastar
+  const onDrag = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      // Armazene a posição inicial do elemento
+      context.startX = translateX.value;
+      context.startY = translateY.value;
+    },
+    onActive: (event, context) => {
+      // Atualize a posição do elemento com base no gesto
+      translateX.value = context.startX + event.translationX;
+      translateY.value = context.startY + event.translationY;
+    },
+    onEnd: () => {
+      console.log('new position:',  translateX.value, translateY.value)
+    }
+  });
 
   const onDoubleTap = useAnimatedGestureHandler({
     onActive: () => {
@@ -25,18 +45,34 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     return {
       width: withSpring(scaleImage.value),
       height: withSpring(scaleImage.value),
+      // transform: [{ translateX: translateX.value }, { translateY: translateY.value }],
+    };
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
     };
   });
 
   return (
-    <View style={{ top: -350 }}>
-      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
-        <AnimatedImage
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </TapGestureHandler>
-    </View>
+    <PanGestureHandler onGestureEvent={onDrag} onHandlerStateChange={onDrag}>
+      <AnimatedView style={[containerStyle, { top: -350 }]}>
+        <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+          <AnimatedImage
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </TapGestureHandler>
+      </AnimatedView>
+    </PanGestureHandler>
   );
 }
